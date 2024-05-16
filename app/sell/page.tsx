9071b299ff5c1917b03a9a8f91bb534bb80a1ca3
Joff317 <j.freixanet@hotmail.com>
2024-05-16 13:03:38 +0200

    private rounting init successfully

--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+import { useLayoutEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
 export default function Sell() {
-  return <div>Sell</div>;
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  useLayoutEffect(() => {
+    if (!session) {
+      router.replace("/login");
+      return;
+    }
+  }, [router, session]);
 }
